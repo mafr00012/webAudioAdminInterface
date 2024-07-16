19592a0d15a0de1c6c7bb177eeeb1fc8317a2ae2
Marc Freidinger <marcfreidinger@gmail.com>
2024-07-16 09:06:35 +0200

    Server und main auf gleichen Stand bringen aber so, dass beide an Ihre Umgebung noch angepasst sind

--- poisView/poisView.js
+++ poisView/poisView.js
@@ -8,7 +8,7 @@ const map = L.map('map', {
   maxBoundsViscosity: 2.0 ,
   minZoom: 3
 })
-const baseURL = BaseURL
+const baseURL = 'https://webaudio.uber.space/api/'
 const breitengradAnzeige = document.getElementById('breitengradAnzeige');
 const laengengradAnzeige = document.getElementById('laengengradAnzeige');
 const nameDisplay = document.getElementById('nameDisplay');
@@ -85,7 +85,10 @@ function loadPoiItems(jumpBackToLastCurrentPoi){
     })
     .catch(error => {
       console.error('Error:', error)
-      window.location.href = '../login.html'
+      const unauthorizedRegex = /.*Unauthorized.*/i
+      if(unauthorizedRegex.test(error.message)) {
+        window.location.href = '../login.html'
+      }
     })
 }
 
@@ -171,7 +174,7 @@ function setCurrentPoi(poi) {
   laengengradAnzeige.textContent = `Längengrad: ${longitude}`
   if(bearbeitungsModusAktiv){
     editUseCaseNameInput.value = poi.databaseData.name
-    setChosenSoundfileId(poi.databaseData.soundfile_id)
+    chosenSoundfileId = poi.databaseData.soundfile_id
     loadSoundfiles()
   }
 
@@ -282,7 +285,7 @@ bearbeitungsModusButton.addEventListener('click', function(){
     markerDatenBearbeitungsmodusClassList.add('markerDatenBearbeitungsmodusVisible');
     bearbeitungsModusAktiv = true;
     editUseCaseNameInput.value = currentPoi.databaseData.name
-    setChosenSoundfileId(currentPoi.databaseData.soundfile_id)
+    chosenSoundfileId = currentPoi.databaseData.soundfile_id
     loadSoundfiles()
   }else{
     markerDatenBearbeitungsmodusClassList.remove('markerDatenBearbeitungsmodusVisible');
@@ -392,8 +395,125 @@ function updatechosenSoundfileCurrentPoi(soundfile){
     .then(response => response.text())
     .then(data => {
       console.log(data)
-      setChosenSoundfileId(soundfile.id)
+      chosenSoundfileId = soundfile.id
       loadSoundfiles()
     })
     .catch(error => console.error('Error:', error));
+}
+
+const dropZone = document.getElementById('drop-zone');
+let chosenSoundfileId = null
+dropZone.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  dropZone.classList.add('dragover');
+});
+
+dropZone.addEventListener('dragleave', () => {
+  dropZone.classList.remove('dragover');
+});
+
+dropZone.addEventListener('drop', (e) => {
+  e.preventDefault();
+  dropZone.classList.remove('dragover');
+
+  const files = e.dataTransfer.files;
+  if (files.length > 0) {
+    const formData = new FormData();
+    formData.append('file', files[0]);
+    fetch(baseURL + "soundfiles", {
+      method:"POST",
+      credentials:"include",
+      body: formData
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log(data)
+        alert(`Datei "${files[0].name}" wurde erfolgreich hochgeladen.`);
+        loadSoundfiles()
+      })
+      .catch(error => {
+        console.error('Error:', error)
+        window.location.href = '../login.html'
+      })
+  }
+});
+
+let abgespielteAudio = null;
+
+const soundfileList = document.getElementById('soundfileList');
+const toggleSoundfileList = document.getElementById('toggleSoundfileList');
+
+function addSoundFileToList(soundfile){
+  const pathToSoundFile = baseURL + `soundfilesFile/${soundfile.filename}`
+  const item = document.createElement('div');
+  item.className = 'soundfile-item';
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.name = 'soundfile';
+  checkbox.value = soundfile.id;
+  if(soundfile.id === chosenSoundfileId){
+    checkbox.checked = true;
+  }
+  checkbox.onclick = () => {
+    document.querySelectorAll('input[name="soundfile"]').forEach(cb => {
+      if (cb !== checkbox){
+        cb.checked = false;
+      } else {
+        updatechosenSoundfileCurrentPoi(soundfile)
+      }
+    });
+  };
+
+  const label = document.createElement('p');
+  label.textContent = soundfile.filename;
+  label.onclick = () => {
+    if(abgespielteAudio != null){
+      abgespielteAudio.pause();
+    }
+    const audio = new Audio(pathToSoundFile);
+    abgespielteAudio = audio;
+    audio.play();
+    setTimeout(() => {
+      abgespielteAudio = null
+      audio.pause();
+    }, 5000);
+  };
+
+  item.appendChild(checkbox);
+  item.appendChild(label);
+  soundfileList.appendChild(item);
+}
+
+function clearSoundfileList(){
+  soundfileList.innerHTML= "";
+}
+
+toggleSoundfileList.addEventListener('click', function() {
+  if (soundfileList.style.display === 'none') {
+    soundfileList.style.display = 'block';
+    dropZone.style.display = 'none';
+    toggleSoundfileList.textContent = 'MP3 hinzufügen';
+  } else {
+    soundfileList.style.display = 'none';
+    dropZone.style.display = 'block';
+    toggleSoundfileList.textContent = 'Zurück zur Liste';
+  }
+});
+
+function loadSoundfiles(){
+  fetch(baseURL + 'soundfiles', {
+    method: "GET",
+    credentials: "include"
+  }).then(response => response.json())
+    .then(data => {
+      clearSoundfileList();
+      data.forEach( value => {
+        addSoundFileToList(value);
+      })
+    })
+    .catch(error => {
+      console.error('Error:', error)
+      window.location.href = '../login.html'
+    })
 }
