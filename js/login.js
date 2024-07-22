a7c55a0bae9b6e62df7cdb424dd6096c55cb5e12
Marc Freidinger <marcfreidinger@gmail.com>
2024-07-22 18:21:38 +0200

    anderer versuch

--- js/login.js
+++ js/login.js
@@ -4,7 +4,21 @@
   const username = document.getElementById('usernameLogin');
   const password = document.getElementById('passwordLogin');
 
-  document.addEventListener("DOMContentLoaded", isLoggedIn)
+  document.addEventListener("DOMContentLoaded", async () =>{
+    try {
+      const data = await isLoggedIn();
+      if (data === 'true') {
+        window.location.href = useCaseSelectionHTMLPath;
+      } else if (data === 'false') {
+        // User is not logged in, no action needed
+      } else {
+        console.error('Unexpected response:', data);
+        alert('Unerwartete Antwort vom Server');
+      }
+    }catch (error) {
+      console.error('Error in DOMContentLoaded listener:', error);
+    }
+  })
 
   loginButton.addEventListener('click', function() {
     const usernameValue = username.value;
@@ -34,24 +48,20 @@
   })
 
   async function isLoggedIn() {
-    fetch(baseURL + 'isAlreadyLoggedin', {
-      method:"GET",
-      headers:{'Content-Type':'application/json'},
-      credentials:"include",
-    })
-      .then(response => response.text())
-      .then(data => {
-        if(data === 'true'){
-          window.location.href = useCaseSelectionHTMLPath;
-        } else if(data === 'false'){
-          // User is not logged in, no action needed
-        } else {
-          console.error('Unexpected response:', data);
-          alert('Unerwartete Antwort vom Server');
-        }
+    try {
+      const response = await fetch(baseURL + 'isAlreadyLoggedin', {
+        method: "GET",
+        headers: {'Content-Type': 'application/json'},
+        credentials: "include",
       })
-      .catch(error => {
-        console.error('Error:', error)
-        alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.');
-      })
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      const data = await response.text();
+      console.log('Login status:', data);
+      return data;
+    } catch (error) {
+      console.error('Error:', error)
+      alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.');
+    }
   }
